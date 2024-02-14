72ff7635d929d832a45da723753f82f19326d15c
Lukas Hutter <el-uhu@users.noreply.github.com>
2024-02-14 18:59:44 +0100

    updated config

--- quartz.config.ts
+++ quartz.config.ts
@@ -10,7 +10,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "dawnchorus.eluhu.xyz",
+    baseUrl: "el-uhu.github.io/dawnchorus",
     ignorePatterns: ["private", "templates", ".obsidian", "meta"],
     defaultDateType: "created",
     theme: {
